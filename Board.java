58d42910cbe96c0501db05daa1dfd32cb3894ed8
MichealMacUistin <mick.hutchinson@hotmail.com>
2017-03-23 17:03:11 +0000

    Add files via upload

---
+++ Board.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Board {
+
+	private Square[] nw = new Square[7];
+	
+	//write a method that checks if there are any empty (null) squares in the array.  Return boolean / number of emptys
+	
+	//write a method that will return a Square array with occupied squares...calcualting amount in phase 3
+	
+	//write a method that will add a blue card to a square (first check there'sspace)
+	
+	//write a method that will remove a drill from a square 
+	
+	//write a method that will add a drill to the square
+	
+	//write a method that assigns a player to a square (blue card)  "won the bid"
+	
+	
+	
+	
+}
