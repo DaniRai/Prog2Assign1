a49458db7e4556ff7c00d4ff5ec4d6efc23df911
margaretmhamilton <magshamilton@hotmail.com>
2017-03-23 16:26:14 +0000

    Add files via upload

--- Players.java
+++ Players.java
@@ -1,44 +1,38 @@
-	import java.util.ArrayList;
+	import java.util.ArrayList;	
 	import java.util.Scanner;
 	
-		//Creating and Declaring the Array of 6 Players
-	public class Players {
+	public class Players {										//Creating and Declaring Array of 6 Players
 		ArrayList<String> players = new ArrayList<>(6);
+		private double currentMoney;							//Current Money from Bank
+		private double currentLoans;							//Current Loans from Bank
+		private double loanRepay;								//Current Loans to be Repaid from Bank
 		
-	public Players()	//Initialization of ArrayList
+	public Players()											//Initialization of ArrayList
 	    {
 	    	players = new ArrayList<String>(6);
 	    }
 		
-	public static void playerCounter(){
+	public static void playerCounter(Scanner input){			//Counter for Players Turn
 			int[]playerCounter = new int[6];
-			int sum =0;
-			
 			for (int i = 0; i < 6; i++)
 			{
-				System.out.print(("It is the turn of Player number : ");
+				System.out.print(("It is the turn of Player number : "));
 				playerCounter[i] = input.nextInt();
-				sum += playerCounter[i];
 			}
-		}
-				
-	public static void turnCounter(){
+		}				
+	public static void turnCounter(Scanner input){				//Counter for Players Turn 1, 2 & 3
 		int[]turnCounter = new int[3];
-		int sum =0;
-		
 		for (int i = 0; i < 3; i++)
 		{
-			System.out.print(("This is turn number : ");
+			System.out.print(("This is turn number : "));
 			turnCounter[i] = input.nextInt();
-			sum += turnCounter[i];
 		}
-	}
-			       
-    public void storePlayer(String player){	//Stores all the players
+	}	
+    public void storePlayer(String player){						//Stores all the players
     	players.add(player);
     }
     
-    public int numberOfPlayers(){		//Shows the number of players
+    public int numberOfPlayers(){								//Shows the number of players
     	return players.size();
     }
     
@@ -50,7 +44,48 @@
     		System.out.println(players.get(playerNumber));
     	}
     }
+    	public void storeCurrentMoney (double currentMoney)		//Store CurrentMoney Figure
+    	{
+    		this.setCurrentMoney(currentMoney);
+    	}
+    	
+    	public void storeCurrentLoans (double currentLoans)		//Store CurrentLoans Figure
+    	{
+    		this.setCurrentLoans(currentLoans);
+    	}
+    	
+    	public void storeLoanRepay (double currentLoans)		//Store LoanRepay Figure
+    	{
+    		double loanRepay = 0;
+			this.setLoanRepay(loanRepay);
+    	}
+
+		public double getCurrentLoans() {						//CurrentLoans Getter
+			return currentLoans;
+		}
+
+		public void setCurrentLoans(double currentLoans) {		//CurrentLoans Setter
+			this.currentLoans = currentLoans;
+		}
+
+		public double getCurrentMoney() {						//CurrentMoney Getter
+			return currentMoney;
+		}
+
+		public void setCurrentMoney(double currentMoney) {		//CurrentMoney Setter
+			this.currentMoney = currentMoney;
+		}
+
+		public double getLoanRepay() {							//LoanRepay Getter
+			return loanRepay;
+		}
+
+		public void setLoanRepay(double loanRepay) {			//LoanRepay Setter
+			this.loanRepay = loanRepay;
+		}
 }
+
+	
 	
 	
 	
